a6632c6b42f5677e6e98c7dec6831ad0406755a7
Griffin Westerman <gxweste@gmail.com>
2025-06-11 09:37:56 -0500

    chore: cleanup

--- lib/instant.ts
+++ lib/instant.ts
@@ -1,14 +1,7 @@
 import { init } from '@instantdb/react';
-// import { init as init_admin } from '@instantdb/admin'; 
 import schema from '@/lib/instant.schema';
 const APP_ID = process.env.NEXT_PUBLIC_INSTANT_APP_ID as string;
-// const ADMIN_TOKEN = process.env.INSTANT_APP_ADMIN_TOKEN as string;
 export const db = init({
   appId: APP_ID,
   schema
 });
-// export const admin_db = init_admin({
-//   appId: APP_ID,
-//   adminToken: ADMIN_TOKEN,
-//   schema
-// })

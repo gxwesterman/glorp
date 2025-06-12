9f77a00618407ce078281be878e2c961ad3ef29b
Griffin Westerman <gxweste@gmail.com>
2025-06-12 17:00:57 -0500

    feat: moved a bunch of stuff from layout to chatcontext

--- app/chat/layout.tsx
+++ app/chat/layout.tsx
@@ -1,52 +1,20 @@
 "use client";
 
-import { db } from "@/lib/instant";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
 import Chat from "@/components/Chat";
 import ChatForm from "@/components/ChatForm";
-import { usePathname } from "next/navigation";
-import { useEffect, useState } from "react";
 import { ChatProvider } from "@/contexts/ChatContext";
 
 export default function Layout() {
-
-  const [sessionId, setSessionId] = useState('');
-  const pathname = usePathname();
-  const pageChatId = pathname.split("/").pop() || "";
-
-  useEffect(() => {
-    const cookies = document.cookie.split(';');
-    const userIdCookie = cookies.find(cookie => cookie.trim().startsWith('g6_session='));
-    const extractedUserId = userIdCookie ? userIdCookie.split('=')[1].trim() : '';
-    setSessionId(extractedUserId);
-  }, [pathname])
-
-  const chatsQuery = {
-    chats: {
-      $: {
-        where: {
-          sessionId: sessionId
-        }
-      },
-      messages: {},
-    },
-  };
-
-  const { isLoading, error, data } = db.useQuery(chatsQuery);
-  if (isLoading) return <div></div>;
-  if (error) return <div>Error fetching data: {error.message}</div>;
-
-  const chat = data.chats.find((chat) => chat.urlId === pageChatId);
-  const messages = chat ? chat.messages : [];
   
   return (
     <SidebarProvider>
-      <AppSidebar chats={data.chats} />
       <ChatProvider>
+        <AppSidebar />
         <main className="relative flex w-full flex-1 flex-col overflow-hidden transition-[width,height]">
-          <Chat messages={messages} />
-          <ChatForm messages={messages} />
+          <Chat />
+          <ChatForm />
         </main>
       </ChatProvider>
     </SidebarProvider>
--- contexts/ChatContext.tsx
+++ contexts/ChatContext.tsx
@@ -1,6 +1,8 @@
 import { db } from "@/lib/instant";
 import { id } from "@instantdb/react";
+import { usePathname } from "next/navigation";
 import { createContext, useContext, useEffect, useState } from "react"
+import { Chat, Message } from "@/lib/types";
 
 function addMessage(text: string, type: string, chatId: string, answerId?: string) {
   db.transact(
@@ -21,10 +23,14 @@ function updateMessage(id: string, content: string) {
 }
 
 type ChatProviderState = {
+  chats: Chat[],
+  messages: Message[],
   startStream: (chatId: string, input: string, messages: { [x: string]: string; id: string; }[]) => Promise<void>
 }
 
 const initialState: ChatProviderState = {
+  chats: [],
+  messages: [],
   startStream: async () => undefined,
 }
 
@@ -34,11 +40,42 @@ export function ChatProvider({
   children,
   ...props
 }: { children: React.ReactNode }) {
-  
+
+  const [sessionId, setSessionId] = useState('');
+  const pathname = usePathname();
+  const pageChatId = pathname.split("/").pop() || "";
+
+  useEffect(() => {
+    const cookies = document.cookie.split(';');
+    const userIdCookie = cookies.find(cookie => cookie.trim().startsWith('g6_session='));
+    const extractedUserId = userIdCookie ? userIdCookie.split('=')[1].trim() : '';
+    setSessionId(extractedUserId);
+  }, [pathname])
+
+  const chatsQuery = {
+    chats: {
+      $: {
+        where: {
+          sessionId: sessionId
+        }
+      },
+      messages: {},
+    },
+  };
+
+  const { isLoading, error, data } = db.useQuery(chatsQuery);
+  if (isLoading) return <div></div>;
+  if (error) return <div>Error fetching data: {error.message}</div>;
+
+  const chats = data.chats;
+  const chat = chats.find((chat) => chat.urlId === pageChatId);
+  const messages = chat ? chat.messages : [];
+
   const startStream = async (chatId: string, input: string, messages: { [x: string]: string; id: string; }[]) => {
     const newAnswerId = id();
     addMessage(input, 'question', chatId);
     addMessage('. . .', 'answer', chatId, newAnswerId);
+
     try {
         const response = await fetch('/api', {
           method: 'POST',
@@ -65,7 +102,7 @@ export function ChatProvider({
           while (!done) {
             const { value, done: readerDone } = await reader.read();
             done = readerDone;
-            result += decoder.decode(value, { stream: true });
+            result = decoder.decode(value, { stream: true });
             updateMessage(newAnswerId, result);
           }
         }
@@ -75,7 +112,7 @@ export function ChatProvider({
   }
 
   return (
-    <ChatProviderContext.Provider {...props} value={{ startStream }}>
+    <ChatProviderContext.Provider {...props} value={{ startStream, chats, messages }}>
       {children}
     </ChatProviderContext.Provider>
   )

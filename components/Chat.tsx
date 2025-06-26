e073b324fbf839d36934280b3613314b092f83bf
Griffin Westerman <gxweste@gmail.com>
2025-06-26 11:24:12 -0500

    feat: add branch component to chat

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -6,6 +6,7 @@ import { useChat } from "@/contexts/ChatContext";
 import ReactMarkdown from "react-markdown";
 import { Dot } from "lucide-react";
 import rehypeHighlight from 'rehype-highlight'
+import Branch from "@/components/Branch";
 
 const pending = (
   <div className="flex" key="pending">
@@ -17,7 +18,7 @@ const pending = (
 
 export default function Chat() {
 
-  const { messages } = useChat();
+  const { chat, messages } = useChat();
   const pathname = usePathname();
   const scrollRef = useRef<HTMLDivElement>(null);
   const streamingAnswer = messages.find(message => message.status === "streaming");
@@ -43,7 +44,7 @@ export default function Chat() {
               <div key={message.id}>
                 {message.type === "question" ? (
                   <div className="flex justify-end">
-                    <div className="group relative inline-block max-w-[80%] break-words rounded-2xl border border-secondary/50 bg-secondary/50 p-4 text-left">
+                    <div className="relative inline-block max-w-[80%] break-words rounded-2xl border border-secondary/50 bg-secondary/50 p-4 text-left">
                       <div className="prose max-w-none prose-pre:m-0 prose-pre:bg-transparent prose-pre:p-0 dark:prose-invert">
                         {message.text}
                       </div>
@@ -57,19 +58,20 @@ export default function Chat() {
                   message.status === "streaming" ?
                   (
                     <div className="flex justify-start">
-                      <div className="group relative w-full max-w-full break-words">
+                      <div className="relative w-full max-w-full break-words">
                         <div className="space-y-4 prose max-w-none prose-pre:m-0 prose-pre:bg-transparent prose-pre:p-0 dark:prose-invert">
                           <ReactMarkdown rehypePlugins={[rehypeHighlight]}>{streamingAnswer?.text}</ReactMarkdown>
                         </div>
                       </div>
                     </div>
                   ) : (
-                    <div className="flex justify-start">
-                      <div className="group relative w-full max-w-full break-words">
+                    <div className="flex flex-col gap-4 justify-start group">
+                      <div className="relative w-full max-w-full break-words">
                         <div className="space-y-4 prose max-w-none prose-pre:m-0 prose-pre:bg-transparent prose-pre:p-0 dark:prose-invert">
                           <ReactMarkdown rehypePlugins={[rehypeHighlight]}>{message.text}</ReactMarkdown>
                         </div>
                       </div>
+                      <Branch className="opacity-0 group-hover:opacity-100" chat={chat} />
                     </div>
                   )
                 )}

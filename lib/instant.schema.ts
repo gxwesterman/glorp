b0fa801ba860964e6068b5e1e38872eb161ef762
Griffin Westerman <gxweste@gmail.com>
2025-06-27 16:18:45 -0500

    feat: added html to instant schema message type

--- lib/instant.schema.ts
+++ lib/instant.schema.ts
@@ -14,6 +14,7 @@ const schema = i.schema({
     messages: i.entity({
       chatId: i.string(),
       text: i.string(),
+      html: i.string(),
       type: i.string(),
       status: i.string().optional(),
     }),

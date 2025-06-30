e0b94a281cbfd14f5ba172c5de610138e64db356
Griffin Westerman <gxweste@gmail.com>
2025-06-30 15:21:02 -0500

    fix: remove observer from chatcontext

--- contexts/ChatContext.tsx
+++ contexts/ChatContext.tsx
@@ -93,38 +93,6 @@ export function ChatProvider({
   const [wasChatPresent, setWasChatPresent] = useState<boolean>(false);
 
   useEffect(() => {
-    const observer = new MutationObserver(() => {
-      const codeBlocks = document.querySelectorAll('[data-code]');
-      if (!codeBlocks?.length) return;
-
-      codeBlocks.forEach((el) => {
-        if (el.hasAttribute('data-hydrated')) return;
-        if (el.getAttribute('data-status') !== 'done') return;
-        el.setAttribute('data-hydrated', 'true');
-        const code = decodeURIComponent(el.getAttribute('data-code') ?? "");
-        const language = el.getAttribute('data-lang');
-        const mountPoint = document.createElement('div');
-        mountPoint.className = "items-center bg-secondary/50 flex justify-between rounded-t-md p-1";
-        el.prepend(mountPoint);
-        const root = createRoot(mountPoint);
-        root.render(
-          <>
-            <span className="font-mono text-muted-foreground ml-2">{language}</span>
-            <Copy content={code} />
-          </>
-        );
-      })
-    });
-
-    observer.observe(document, {
-      childList: true,
-      subtree: true,
-    });
-
-    return () => observer.disconnect();
-  }, []);
-
-  useEffect(() => {
     if (chat && chat.id !== "chat") {
       setWasChatPresent(true);
     }

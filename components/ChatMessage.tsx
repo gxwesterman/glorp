9fe30016b6e4a865d4d525fc946278ec09ddbcce
Griffin Westerman <gxweste@gmail.com>
2025-07-03 13:54:04 -0500

    feat: tedious method of coloring bulletpoints teal

--- components/ChatMessage.tsx
+++ components/ChatMessage.tsx
@@ -17,8 +17,7 @@ export const ChatMessage = memo(function ChatMessage({ message, chat, index }: {
   return (
     <div className="flex flex-col justify-start group">
       <div className="relative w-full max-w-full break-words">
-        <div className="space-y-4 prose max-w-none prose-pre:m-0 prose-pre:bg-transparent prose-pre:p-0 dark:prose-invert">
-          {/* <div dangerouslySetInnerHTML={{ __html: message.html }} /> */}
+        <div className="space-y-4 prose max-w-none prose-pre:m-0 prose-pre:bg-transparent prose-teal prose-pre:p-0 dark:prose-invert">
           {chunks.map((chunk, index) => (
             <Chunk key={index} html={chunk} />
           ))}

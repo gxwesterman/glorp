9d8691ad6fe0a28b2792fa9b52e20e00ac8f412f
Griffin Westerman <gxweste@gmail.com>
2025-07-09 16:16:01 -0500

    feat: color prose counters too

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -4,7 +4,8 @@ module.exports = {
       typography: () => ({
         teal: {
           css: {
-            '--tw-prose-invert-bullets': 'var(--color-teal-600)',
+            '--tw-prose-invert-bullets': 'var(--color-teal-500)',
+            '--tw-prose-invert-counters': 'var(--color-teal-500)',
           },
         },
       }),
